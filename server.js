9fcf020649ceb8690e44a886fe2e3f9a100f8ee7
Kaeneth Dayao <kendayao@gmail.com>
2020-05-22 16:08:03 -0700

    set up app.get to api notes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var fs = require("fs");
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -11,10 +12,22 @@ console.log("Server is running on http://localhost:" + PORT)
 });
 
 
-app.get("/",function(req, res){
-res.sendFile(__dirname + "/index.html")
-});
 
 app.get("/notes", function(req,res){
 res.sendFile(__dirname + "/notes.html")
+});
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname + "/index.html")
+    });
+
+app.get("/api/notes",function(req, res){
+   
+      
+      fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data)
+        res.json(data)
+      });
+     
 });
