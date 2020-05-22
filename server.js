893720c80013bc2af9068d7ce60cf06bcc5f021b
Kaeneth Dayao <kendayao@gmail.com>
2020-05-21 19:28:27 -0700

    app.get setup for hoempage and notes.html

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
-
 console.log("Server is running on http://localhost:" + PORT)
 });
 
+
+app.get("/",function(req, res){
+res.sendFile(__dirname + "/index.html")
+});
+
+app.get("/notes", function(req,res){
+res.sendFile(__dirname + "/notes.html")
+});
